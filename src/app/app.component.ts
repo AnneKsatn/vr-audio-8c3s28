67f7868afb8d98ce2a1d285c39665eb7e95efe2b
Kasatkina <anna.kasatkina@intel.com>
2022-03-27 15:19:42 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare const MediaRecorder: any;
+declare const navigator: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public isRecording: boolean = false;
+  private chunks: any = [];
+  private mediaRecorder: any;
+
+  constructor() {
+    const onSuccess = stream => {
+      this.mediaRecorder = new MediaRecorder(stream);
+      this.mediaRecorder.onstop = e => {
+        const audio = new Audio();
+        const blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' });
+        this.chunks.length = 0;
+        audio.src = window.URL.createObjectURL(blob);
+        audio.load();
+        audio.play();
+
+        this.saveAudio(blob)
+      };
+
+      this.mediaRecorder.ondataavailable = e => this.chunks.push(e.data);
+    };
+
+    navigator.getUserMedia = (navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia);
+
+    navigator.getUserMedia({ audio: true }, onSuccess, e => console.log(e));
+  }
+
+  public record() {
+    this.isRecording = true;
+    this.mediaRecorder.start();
+  }
+
+  public stop() {
+    this.isRecording = false;
+    this.mediaRecorder.stop();
+  }
+
+  async saveAudio(audioBlob) {
+
+    const formData = new FormData()
+
+    let audioName;
+    audioName = "command" ? Date.now() + '-' + audioName : Date.now()
+
+    formData.append('audio', audioBlob, audioName)
+
+    try {
+      await fetch('http://localhost:3000/save', {
+        method: 'POST',
+        body: formData
+      })
+
+      console.log('Saved')
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }

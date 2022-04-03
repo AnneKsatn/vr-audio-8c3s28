bd78d0977f8b12495ecfebeef5c185312a95c1bf
AnneKsatn <36439886+AnneKsatn@users.noreply.github.com>
2022-04-03 21:50:33 +0300

    audio done, video - question

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 declare const MediaRecorder: any;
 declare const navigator: any;
+import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +10,87 @@ declare const navigator: any;
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  //video
+  public showWebcam = true;
+  public allowCameraSwitch = true;
+  public multipleWebcamsAvailable = false;
+  public deviceId: string;
+
+  //audio
   public isRecording: boolean = false;
   private chunks: any = [];
   private mediaRecorder: any;
+
+  public errors: WebcamInitError[] = [];
+
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
+
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+  // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
+  private nextWebcam: Subject<boolean | string> = new Subject<boolean | string>();
+
+  public ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+      });
+  }
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  public showNextWebcam(directionOrDeviceId: boolean | string): void {
+    // true => move forward through devices
+    // false => move backwards through devices
+    // string => move to device with given deviceId
+    this.nextWebcam.next(directionOrDeviceId);
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+
+    const arr = this.webcamImage.imageAsDataUrl.split(",");
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    const file: File = new File([u8arr], "file", { type: 'img/pmg' })
+    console.log(file);
+
+    this.saveImage(file)
+  }
+
+  public cameraWasSwitched(deviceId: string): void {
+    console.log('active device: ' + deviceId);
+    this.deviceId = deviceId;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<boolean | string> {
+    return this.nextWebcam.asObservable();
+  }
 
   constructor() {
     const onSuccess = stream => {
       this.mediaRecorder = new MediaRecorder(stream);
       this.mediaRecorder.onstop = e => {
         const audio = new Audio();
-        const blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' });
+        const blob = new Blob(this.chunks, { 'type': 'video/mp4' });
         this.chunks.length = 0;
         audio.src = window.URL.createObjectURL(blob);
         audio.load();
@@ -34,7 +107,7 @@ export class AppComponent {
       navigator.mozGetUserMedia ||
       navigator.msGetUserMedia);
 
-    navigator.getUserMedia({ audio: true }, onSuccess, e => console.log(e));
+    navigator.getUserMedia({ video: true }, onSuccess, e => console.log(e));
   }
 
   public record() {
@@ -52,12 +125,28 @@ export class AppComponent {
     const formData = new FormData()
 
     let audioName;
-    audioName = "command" ? Date.now() + '-' + audioName : Date.now()
 
-    formData.append('audio', audioBlob, audioName)
+    formData.append('file', audioBlob, 'file')
 
     try {
-      await fetch('http://localhost:3000/save', {
+      await fetch('http://localhost:5000/save', {
+        method: 'POST',
+        body: formData
+      })
+
+      console.log('Saved')
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async saveImage(audioBlob) {
+
+    const formData = new FormData()
+    formData.append('file', audioBlob)
+
+    try {
+      await fetch('http://localhost:5000/save_image', {
         method: 'POST',
         body: formData
       })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
+import { Media} from '@ionic-native/media'
+import { HttpClientModule } from '@angular/common/http';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot()],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, WebcamModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Media],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
